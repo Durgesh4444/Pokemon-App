108b7b7eb87205d2aea8d492c80001d40cd0b908
Durgesh_Sonkusare <7276104188d@gmail.com>
2023-05-04 20:23:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from 'react'
+import './index.css'
+import './App.css'
+import Axios from 'axios'
+import Pokemon from './component/Pokemon';
+import img from "./component/pokemonlogo.png"
+
+const poklogo = img;
 
 function App() {
+  const [text, setText] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const fetechPokemons = async (e) => {
+
+   e.preventDefault();
+
+
+    if(search === ""){
+         alert("Please Enter Any Data ")
+         return;
+    }
+
+         const {data} = await Axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`)
+         console.log("Response", data);
+
+         setText(data)
+         
+  }
+
+  useEffect( ()=> {
+    fetch(`https://pokeapi.co/api/v2/pokemon/`)
+}, [search])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <>
+        <img className='logo' src={poklogo} alt='img'></img>
+        <form className='pokemonForm'>
+             <input type="text" value={search} placeholder='Enter Any Pokemon Name' onChange={(e) => setSearch(e.target.value)}/>
+             <button onClick={fetechPokemons}>Search</button>
+        </form>
+        <Pokemon text={text}/>
+     </>
+  )
 }
 
 export default App;
